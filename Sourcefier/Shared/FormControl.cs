7b2bc9e9a6dd11dde9e2ddd5dc9e24e02f937716
Antti Veikkolainen <aveboy.00@gmail.com>
2023-10-04 21:30:46 +0300

    Initial structure

---
+++ Sourcefier/Shared/FormControl.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Sourcefier.Shared;
+
+public class FormControl<T> : ComponentBase
+{
+    [Parameter]
+    public T Property { get; set; }
+
+    [Parameter]
+    public EventCallback<T> PropertyChanged { get; set; }
+
+    [Parameter]
+    public string TooltipText { get; set; }
+
+    [Parameter]
+    public string Placeholder { get; set; }
+
+    [Parameter]
+    public string Label { get; set; }
+
+    [Parameter]
+    public string LabelText { get; set; }
+
+    [Parameter]
+    public string Id { get; set; }
+}
