efcaf0bccc85df1d1b5a947ec05a5eee34274d29
Antti Veikkolainen <antti.veikkolainen00@outlook.com>
2023-10-08 01:54:38 +0300

    Add language navigation

--- Sourcefier/Pages/Index.razor.cs
+++ Sourcefier/Pages/Index.razor.cs
@@ -1,11 +1,43 @@
-﻿using Sourcefier.DTO.Enums;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Sourcefier.DTO.Enums;
 
 namespace Sourcefier.Pages;
 
 public partial class Index
 {
+
+    #region Properties
+
+    [Inject]
+    private NavigationManager _navigationManager { get; init; } = default!;
+
+    [Parameter]
+    [SupplyParameterFromQuery]
+    public string? Lang { get; set; }
+
     protected Mode Mode { get; set; } = Mode.Basic;
 
     protected Language Language { get; set; } = Language.Finnish;
 
+    #endregion
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (string.IsNullOrWhiteSpace(Lang))
+            return;
+
+        if (Lang == "fi")
+            Language = Language.Finnish;
+        else if (Lang == "en")
+            Language = Language.English;
+    }
+
+    protected void OnLangaugeChange(Language language)
+    {
+        Language = language;
+
+        string lang = language == Language.Finnish ? "fi" : "en";
+        _navigationManager.NavigateTo($"?lang={lang}", false);
+    }
 }
